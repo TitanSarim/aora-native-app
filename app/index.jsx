84d7e2e99ca4ec79e7f93705c2cef3a510c5865a
TitanSarim <muhammadsarim203@gmail.com>
2024-05-19 23:01:57 +0500

    sarim commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import {StyleSheet, View, Text } from "react-native";
+import { View, Text } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
 
   return(
-    <View style={styles.container}>
-      <Text>Aora</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora</Text>
       <StatusBar style="auto"/>
       <Link href="/profile">Go to Profile</Link>
     </View>
@@ -14,13 +14,3 @@ export default function App() {
 
 }
 
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-
-})
