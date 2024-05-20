4b229d9b6c2a9c7680c7f82879c0a22bb6bf2541
TitanSarim <muhammadsarim203@gmail.com>
2024-05-21 02:16:05 +0500

    sarim commmit

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { View, Text, Image, ScrollView } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 export default function App() {
 
   return(
@@ -19,8 +20,10 @@ export default function App() {
             <Image source={images.path} className="w-[136px] h-[15px] absolute -bottom-2 -right-8" resizeMode="contain"/>
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+          <CustomButton title="Countinue with Email" handlePress={() => router.push('/sign-in')} containerStyles="w-full mt-7"/>
         </View>
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light'/>
 
     </SafeAreaView>
   )
